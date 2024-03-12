8a06e2ff6428542f15e2fefa0c8f46c6e826e415
NandaSasidharan <sr.nanda.k@gmail.com>
2024-03-12 16:09:36 +0100

    fixed scrollbar and added functionality to show the meaning of base word 'level2_definition_word'

--- reader_dict_app.py
+++ reader_dict_app.py
@@ -41,8 +41,8 @@ class Application(tk.Frame):
         self.story.grid(row=0, column=0, columnspan=2, sticky=tk.EW, padx=5, pady=5)
 
         # create a scrollbar widget and set its command to the text widget
-        self.scrollbar = ttk.Scrollbar(root, orient='vertical', command=self.story.yview)
-        self.scrollbar.grid(row=0, column=2, sticky=tk.NS, rowspan=1)
+        self.scrollbar = ttk.Scrollbar(self, orient='vertical', command=self.story.yview)
+        self.scrollbar.grid(row=0, column=2, sticky=tk.NS)
         #  communicate back to the scrollbar
         self.story['yscrollcommand'] = self.scrollbar.set
 
@@ -53,10 +53,10 @@ class Application(tk.Frame):
         # Create a label saying how to search for meaning
         self.label = ttk.Label(self, text='Click at the word beginning or search here:', font=custom_font,
                                foreground='gray', anchor=tk.W)
-        self.label.grid(row=1, column=0, sticky=tk.W, padx=5, pady=5)
+        self.label.grid(row=1, column=0, sticky=tk.E, padx=5, pady=5)
         # Create an Entry widget for user input
         self.entry = tk.Entry(self, font=custom_font)
-        self.entry.grid(row=1, column=1, columnspan=2, sticky=tk.EW,  padx=5, pady=5)
+        self.entry.grid(row=1, column=1, sticky=tk.W,  padx=5, pady=5)
         # Bind the Return key to the show_definition method
         self.entry.bind('<Return>', self.show_definition_from_entry)
 
@@ -92,15 +92,35 @@ class Application(tk.Frame):
         word = self.entry.get().strip()
         self.show_definition(word)
 
-    def show_definition(self, word):
-        self.definition.delete('1.0', tk.END)
+    def show_definition(self, word, clear=True):
+        if clear:
+            self.definition.delete('1.0', tk.END)
         definition_label = f"Meaning of '{word}': \n"
         self.definition.insert("end", definition_label)
         if word in self.my_dict.keys():
             definition = self.my_dict[word]
             self.insert_clickable(definition, self.definition)
+            level2_word = self.level2_definition_word(definition)
+            if level2_word and level2_word!=word: # check if level2word is None and if it is same as word (leads to endless recursion)
+                self.show_definition(level2_word, clear=False)
         else:
             self.definition.insert("end", "No definition found.")
+
+    def level2_definition_word(self, definition):
+        ''' sometimes the definition only shows that the word is a grammatical variation 
+        of another base word. This function helps to isolate this base word.'''
+        level2_indicators = ['inflection', 'participle', 'preterite', 'imperative', 'plural', 'singular', 'spelling', 'equivalent', 'degree', 'genitive']
+        for indicator in level2_indicators:
+            for sentence in definition[1:]:
+                words = sentence.split()
+                if indicator in words:
+                    indicator_index = words.index(indicator)
+                    if indicator_index < len(words) - 2:
+                        word = words[indicator_index + 2]
+                        word = word.replace(':', '')
+                        return word
+   
+                
 
 
 root = tk.Tk()
